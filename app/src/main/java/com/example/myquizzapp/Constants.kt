885db8e2b06a40b7ae8872498d3f9aa9f365dccd
Cesar Romero <zaesarpo@gmail.com>
2021-12-09 10:56:33 +0100

    Added underscore

--- app/src/main/java/com/example/myquizzapp/Constants.kt
+++ app/src/main/java/com/example/myquizzapp/Constants.kt
@@ -2,7 +2,7 @@ package com.example.myquizzapp
 
 object Constants {
 
-    const val USER_NAME : String ="user:name"
+    const val USER_NAME : String ="user_name"
     const val TOTAL_QUESTIONS :String = "total_questions"
     const val CORRECT_ANSWERS : String = "correct_answers"
 
